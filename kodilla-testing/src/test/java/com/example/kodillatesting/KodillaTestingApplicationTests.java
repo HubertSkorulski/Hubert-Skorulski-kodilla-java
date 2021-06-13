cae92efc83b35c1f2075ea4478e0968847c1e629
huubsoon <hubertskorulski@gmail.com>
2021-06-13 14:51:32 +0200

    6.3 Odd Ext

--- kodilla-testing/src/test/java/com/example/kodillatesting/KodillaTestingApplicationTests.java
+++ kodilla-testing/src/test/java/com/example/kodillatesting/KodillaTestingApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.kodillatesting;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class KodillaTestingApplicationTests {
 
     @Test
