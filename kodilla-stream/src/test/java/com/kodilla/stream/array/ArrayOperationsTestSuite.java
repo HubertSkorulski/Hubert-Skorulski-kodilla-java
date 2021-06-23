3b49ca15b38cae4616163fb03bc26b78a4249848
huubsoon <hubertskorulski@gmail.com>
2021-06-23 09:18:03 +0200

    Poprawione 7.4 i 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.stream.array;
+
+import org.junit.jupiter.api.Test;
+import java.util.OptionalDouble;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite {
+@Test
+    void testGetAverage() {
+    //Given
+    int[] nums = {2,4,2,2,2,2,2,2,2,4,4,4,4,47,3,3,4,2,4,5};
+    //When
+    OptionalDouble result = ArrayOperations.getAverage(nums);
+    //Then
+    assertEquals(5.2,result.getAsDouble());
+}
+}
--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -4,15 +4,17 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WorldTestSuite {
     @Test
-    void testGetSandBeansQuantity() {
+    void testGetPeopleQuantity() {
         //Given
         Continent southAmerica = new Continent();
         Continent europe = new Continent();
         Country poland = new Country(new BigDecimal("210000000000000000000"));
         Country germany = new Country(new BigDecimal("310000000000000000000"));
-        Country uruguay = new Country(new BigDecimal("410000000000000000000"));
+        Country uruguay = new Country(new BigDecimal("410000000000000000001"));
         Country rpa = new Country(new BigDecimal("510000000000000000000"));
         //When
         southAmerica.addCountry(uruguay);
@@ -22,8 +24,10 @@ public class WorldTestSuite {
         World world = new World();
         world.addContinent(southAmerica);
         world.addContinent(europe);
-        //Then
         BigDecimal people = world.getPeopleQuantity();
-        System.out.println(people);
+        BigDecimal result = new BigDecimal("1440000000000000000001");
+        //Then
+        assertEquals(result,people);
+
     }
 }
