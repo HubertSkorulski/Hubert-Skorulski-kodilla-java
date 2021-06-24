dc306c189c4f8cdf44dcd0e02f99b058f6c6c45a
huubsoon <hubertskorulski@gmail.com>
2021-06-24 09:23:02 +0200

    7.6. Wykorzystanie Stream na różnych kolekcjach

--- kodilla-testing/src/main/java/FreeStyleChecking.java
+++ kodilla-testing/src/main/java/FreeStyleChecking.java
@@ -1,10 +1,14 @@
 import java.time.LocalDate;
 
+import static java.time.temporal.ChronoUnit.DAYS;
+
 public class FreeStyleChecking {
     public static void main(String[] args){
         LocalDate date = LocalDate.of(2019,8,19);
-        LocalDate date2 = LocalDate.of(2019,9,19);
-        LocalDate date3 = LocalDate.of(LocalDate.now().getYear()-20,LocalDate.now().getMonth(),LocalDate.now().getDayOfMonth());
+        LocalDate date2 = LocalDate.of(2019,9,17);
+        double date3 = DAYS.between(date,date2);
         System.out.println(date3);
+
+
     }
 }
